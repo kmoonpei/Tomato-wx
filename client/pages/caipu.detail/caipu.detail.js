84469aef863cc7ffd7076f21a49117905e00f695
Yuki <18482177461@163.com>
2019-04-19 18:29:59 +0800

    收藏

--- client/pages/caipu.detail/caipu.detail.js
+++ client/pages/caipu.detail/caipu.detail.js
@@ -4,9 +4,18 @@ Page({
     data: {
         id: null,
         data: {},
+        collected:true,
     },
     onLoad(option) {
         console.log('onLoad id:', option.id)
+        wx.getStorage({
+            key: 'uid',
+            success: (result) => {
+                this.setData({ uid: result.data })
+            },
+            fail: () => { },
+            complete: () => { }
+        });
         this.setData({ id: option.id }, () => {
             this.getData();
         })
@@ -23,6 +32,19 @@ Page({
                     dat.material = JSON.parse(dat.material);
                     dat.steps = JSON.parse(dat.steps);
                     this.setData({ data: dat });
+                }
+            })
+    },
+    collect() {
+        let { uid, id } = this.data;
+        let payload = {
+            uid: uid,
+            cpid: id
+        }
+        http.requestPost('/caipu/collect', payload)
+            .then(data => {
+                if (data.data.code === 0) {
+                    this.setData({collected:!this.data.collected})
                 }
             })
     }
