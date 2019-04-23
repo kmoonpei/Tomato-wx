2753eb09d8bcc038b7d5f0dc5e0844d9aaf251d2
Yuki <18482177461@163.com>
2019-04-23 18:15:19 +0800

    修改详情页

--- client/pages/caipu.detail/caipu.detail.js
+++ client/pages/caipu.detail/caipu.detail.js
@@ -4,7 +4,10 @@ Page({
     data: {
         id: null,
         data: {},
-        collected:true,
+        collect_count: null,
+        collected: true,
+        clr_collec_btn: '#6cb63b',
+        collect_str: '收藏'
     },
     onLoad(option) {
         console.log('onLoad id:', option.id)
@@ -21,8 +24,10 @@ Page({
         })
     },
     getData() {
+        let { uid, id } = this.data;
         let payload = {
-            id: this.data.id
+            cpid: id,
+            uid: uid
         }
         http.requestPost('/caipu/detail', payload)
             .then(data => {
@@ -31,7 +36,13 @@ Page({
                     let dat = data.data.data;
                     dat.material = JSON.parse(dat.material);
                     dat.steps = JSON.parse(dat.steps);
-                    this.setData({ data: dat });
+                    this.setData({
+                        data: dat,
+                        collect_count: dat.collect_count,
+                        collected: dat.is_collected,
+                        clr_collec_btn: !dat.is_collected ? '#6cb63b' : '#aaa',
+                        collect_str: !dat.is_collected ? '收藏' : '取消收藏',
+                    });
                 }
             })
     },
@@ -44,7 +55,12 @@ Page({
         http.requestPost('/caipu/collect', payload)
             .then(data => {
                 if (data.data.code === 0) {
-                    this.setData({collected:!this.data.collected})
+                    this.setData({
+                        collected: !this.data.collected,
+                        collect_count: data.data.msg == "收藏成功" ? ++this.data.collect_count : --this.data.collect_count,
+                        collect_str: data.data.msg == "收藏成功" ? '取消收藏' : '收藏',
+                        clr_collec_btn: this.data.clr_collec_btn == '#6cb63b' ? '#aaa' : '#6cb63b'
+                    })
                 }
             })
     }
